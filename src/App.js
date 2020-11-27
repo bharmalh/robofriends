9be526de3ed8cdd44cbeb4d551f41e731866f6c7
Hussain Bharmal <47472219+bharmalh@users.noreply.github.com>
2020-11-27 21:57:02 +0300

    function react to class react

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+class App extends React.Component{
+  render() {
+    return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -19,7 +21,8 @@ function App() {
         </a>
       </header>
     </div>
-  );
+    )
+  }
 }
 
 export default App;
