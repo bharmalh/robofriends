b3d96d5fa79412828bd8ba6036d58307c082796e
Hussain Bharmal <47472219+bharmalh@users.noreply.github.com>
2021-04-16 23:37:59 +0300

    updating libraries

---
+++ src/containers/App.js
@@ -0,0 +1,48 @@
+import React, {Component} from 'react';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
+import './App.css';
+// import {robots} from './robots';
+
+class App extends Component {
+	constructor(){
+		super()
+		this.state={
+			robots: [],
+			searchField: ''
+		}
+	}
+
+componentDidMount(){
+	fetch('https://jsonplaceholder.typicode.com/users')
+	.then(response=>response.json())
+	.then(users => this.setState({robots: users}));
+}
+
+onSearchChange = (event) => {
+	this.setState({searchField: event.target.value})
+}
+
+	render() {
+		const {robots, searchField} = this.state;
+		const filterRobots = robots.filter(robot =>{
+			return robot.name.toLowerCase().includes(searchField.toLowerCase())
+	})
+	return !robots.length ?
+ <h1>Loading...</h1> :
+ 
+(
+		<div className='tc'>
+		<h1 className='f1'>Robofriends</h1>
+		<SearchBox searchChange={this.onSearchChange}/>
+		<Scroll>
+		<CardList robots={filterRobots}/>
+		</Scroll>
+		</div>
+	);
+	}
+}
+
+
+export default App;
